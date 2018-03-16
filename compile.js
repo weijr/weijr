682421a50bb672611419deb7bd80eb99c740953b
Marko Lisonek <mlisonek98@gmail.com>
2018-03-16 13:14:53 -0400

    feat/contract/basic-contract-works

---
+++ compile.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const mafiaPath = path.resolve(__dirname, "contracts", "Mafia.sol");
+const source = fs.readFileSync(mafiaPath, "utf8");
+
+// module.exports = solc.compile(source, 1).contracts[:Mafia]
