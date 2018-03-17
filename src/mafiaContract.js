eb0663f4381d3333ee4e7cae91bf9558a35990b6
Ildi Bilali <bilaliildi@gmail.com>
2018-03-17 14:25:34 -0400

    added abi and changed deploy for contract to prep for contract front end

---
+++ src/mafiaContract.js
@@ -0,0 +1,110 @@
+import web from "./web3";
+
+const address = "0x97514887629A0c6f6853c34B588CE338F7727d7F";
+
+const abi = [
+  {
+    constant: false,
+    inputs: [
+      { name: "currentPlayer", type: "address" },
+      { name: "mafia", type: "bool" }
+    ],
+    name: "addPlayer",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "mafiaMembers",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "villagerMembers",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [{ name: "playerThatsPlaying", type: "address" }],
+    name: "recordPlayerIsPlaying",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [{ name: "isWinningTeamMafia", type: "bool" }],
+    name: "payout",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [{ name: "isMafia", type: "bool" }],
+    name: "incrementSides",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayersLength",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "address" }],
+    name: "alreadyPlaying",
+    outputs: [{ name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [
+      { name: "gamePlayer", type: "address" },
+      { name: "mafia", type: "bool" }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor"
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
