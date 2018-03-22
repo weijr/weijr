758e52ba8e9955ba868769dfd4646e0941423d11
Marko Lisonek <mlisonek98@gmail.com>
2018-03-22 13:07:51 -0400

    preparing for merge

--- src/ether/factory.js
+++ src/ether/factory.js
@@ -3,7 +3,7 @@ import WagerFactory from './build/WagerFactory';
 
 const instance = new web3.eth.Contract(
   JSON.parse(WagerFactory.interface),
-  '0xDc3903ADc648Fe951653a6244cBd629bbe991355'
+  '0x0a304fbf1C430E93eca77b7A30B2F49a773337B5'
 );
 
 export default instance;
