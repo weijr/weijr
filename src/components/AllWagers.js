f1181569daeb74f4c4d2a7e64a95fe40a73e307b
Anthony Huang <ahuang1822@gmail.com>
2018-03-23 15:08:16 -0400

    fix(get-all-contract-info) await

--- src/components/AllWagers.js
+++ src/components/AllWagers.js
@@ -30,45 +30,40 @@ class AllWagers extends Component {
     this.logout = this.logout.bind(this)
   }
 
-  // async componentDidMount() {
-  //   let listOfWagersInfo = []
-    
-  //   let listOfWagersAddresses = await factory.methods.getDeployedwagers().call()
-  //   await listOfWagersAddresses.map(async address => {
-  //     let wager = Wager(address)
-  //     let info = await wager.methods.getWagerSummary().call()
-  //     //console.log("info: ", info)
-  //     let summary = {
-  //       title: info[6],
-  //       address: info[5]
-  //     }
-  //     listOfWagersInfo.push(info)
-  //   })
-  //   let listOfWagersInfoSlice = listOfWagersInfo.slice(0)
-  //   this.setState({
-  //     listOfWagers: listOfWagersInfoSlice
-  //   })
-  // }
-
-  componentDidMount() {
-    let listOfWagersInfo = []
-    factory.methods.getDeployedwagers().call()
-      .then(listOfAddress => {
-        Promise.all(listOfAddress.map(address => {
-          let wager = Wager(address)
-          wager.methods.getWagerSummary().call()
-            .then(info => {
-              console.log('info: ', info)
-              return listOfWagersInfo.push(info)
-            })
-        }))
-          .then(() => {
-            this.setState({
-              listOfWagers: listOfWagersInfo
-            })
-          })
+  async componentDidMount() {
+    try {
+      const listOfWagersAddresses = await factory.methods.getDeployedwagers().call()
+      const listOfWagers = await Promise.all(listOfWagersAddresses.map(address => {
+        const wager = Wager(address)
+        return wager.methods.getWagerSummary().call()
+      }))
+      this.setState({
+        listOfWagers
       })
+    } catch (err) {
+      console.error(err)
+    }
   }
+
+  // componentDidMount() {
+  //   let listOfWagersInfo = []
+  //   factory.methods.getDeployedwagers().call()
+  //     .then(listOfAddress => {
+  //       return Promise.all(listOfAddress.map(address => {
+  //         let wager = Wager(address)
+  //         wager.methods.getWagerSummary().call()
+  //           .then(info => {
+  //             console.log('info: ', info)
+  //             return listOfWagersInfo.push(info)
+  //           })
+  //       }))
+  //         .then(values => {
+  //           this.setState({
+  //             listOfWagers: values
+  //           })
+  //         })
+  //     })
+  // }
 
 
   signUp = event => {
@@ -78,14 +73,14 @@ class AllWagers extends Component {
 
   logout = () => {
     auth.signOut()
-    .then(() => {
-      this.setState({
-        currentUser: ""
+      .then(() => {
+        this.setState({
+          currentUser: ""
+        })
       })
-    })
-    .then(() => {
-      this.props.history.push('/')
-    })
+      .then(() => {
+        this.props.history.push('/')
+      })
   }
 
   createContract = (event) => {
@@ -108,44 +103,49 @@ class AllWagers extends Component {
       return (
         // this.state.listOfWagers.length === 0 ? null :
         <div>
-        <Segment inverted>
-          <Header inverted as="h2" icon textAlign="center">
-            <Icon name="ethereum" circular />
-            <Header.Content>
-              <h2 className="ui red header">
-                Welcome 2 Wagr
+          <Segment inverted>
+            <Header inverted as="h2" icon textAlign="center">
+              <Icon name="ethereum" circular />
+              <Header.Content>
+                <h2 className="ui red header">
+                  Welcome 2 Wagr
             </h2>
-            </Header.Content>
-            <Header.Content>
-              <h2 className="ui red header">
-                See a wager that you like? Sign in to see more!
+              </Header.Content>
+              <Header.Content>
+                <h2 className="ui red header">
+                  See a wager that you like? Sign in to see more!
               </h2>
-            </Header.Content>
-            <Header.Content>
-              <Button onClick={this.logout}>
-                Logout
+              </Header.Content>
+              <Header.Content>
+                <Button onClick={this.logout}>
+                  Logout
               </Button>
               </Header.Content>
               <Header.Content>
-              <Button onClick={this.createContract}>
-                Create a new Contract
+                <Button onClick={this.createContract}>
+                  Create a new Contract
               </Button>
-            </Header.Content>
-          </Header>
-        </Segment>
-        <Grid columns={5}>
-        {wagerList.map(wager => (
-          <Grid.Column>
-            <Card key={wager.title} className="ui segment centered">
-              <Image src={basketball} />
-              <Card.Header />
-              <Link to={`/wagers/${wager.address}`} key={wager.address} value={wager.address}>
-                Click here to bet on {wager.title}
-              </Link>
-            </Card>
-          </Grid.Column>
-        ))}
-        </Grid>
+              </Header.Content>
+            </Header>
+          </Segment>
+          <Grid columns={5}>
+            {wagerList.map(wager => (
+              <Grid.Column>
+                <Card key={wager[5]} className="ui segment centered">
+                  <Image src={basketball} />
+                  <Card.Header />
+                  <Link to={`/wagers/${wager[5]}`} key={wager[5]} value={wager[5]}>
+                    Click here to bet on
+                    <br></br>
+                    {wager[6]}
+                  </Link>
+                  Ante: {wager[0]} Ether
+                  <br></br>
+                  Current Pot Size: {wager[1]}
+                </Card>
+              </Grid.Column>
+            ))}
+          </Grid>
         </div>
       )
     } else if (this.state.currentUser) {
@@ -155,18 +155,18 @@ class AllWagers extends Component {
     } else {
       this.props.history.push('/')
       return null
-      }
     }
   }
+}
 
-  const mapStateToProps = function(state, ownProps) {
-    return {
-      currentUser: state.user
-    };
+const mapStateToProps = function (state, ownProps) {
+  return {
+    currentUser: state.user
   };
+};
 
-  const mapDispatchToProps = function(dispatch, ownProps) {
-    return {
+const mapDispatchToProps = function (dispatch, ownProps) {
+  return {
   }
 }
 
