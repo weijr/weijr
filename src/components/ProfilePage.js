fd5f9f730227710a22301a639da2a830845d59c4
Sunny Kim <Sunny@Sunnys-MacBook-Pro.local>
2018-03-27 20:34:34 -0400

    merging w master

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -7,59 +7,66 @@ import history from "../history";
 import store from "../store";
 import { browserHistory } from "react-router";
 import basketball from "./basketball.png";
-import { Header, Icon, Image, Segment, Grid, Button, Card, Dropdown } from 'semantic-ui-react'
+import {
+  Header,
+  Icon,
+  Image,
+  Segment,
+  Grid,
+  Button,
+  Card,
+  Dropdown
+} from "semantic-ui-react";
 import { connect } from "react-redux";
 import Wager from "../ether/wagers";
 import web3 from "../ether/web3";
 
-
 class ProfilePage extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       wager: this.props.wager,
       metamask: "",
       listOfWagers: []
-    }
-    this.goHome = this.goHome.bind(this)
+    };
+    this.goHome = this.goHome.bind(this);
   }
 
-  goHome = (event) => {
-    this.props.history.push('/wagers')
-  }
+  goHome = event => {
+    this.props.history.push("/wagers");
+  };
 
   async componentDidMount() {
     try {
-      const accounts = await web3.eth.getAccounts()
+      const accounts = await web3.eth.getAccounts();
       this.setState({
         metamask: accounts[0]
-      })
+      });
     } catch (err) {
       console.error(err);
     }
   }
 
-
   render() {
-    console.log("metamask: ", this.props.currentUser)
-    console.log("props: ", this.props.listOfWagers)
+    console.log("metamask: ", this.props.currentUser);
+    console.log("props: ", this.props.listOfWagers);
 
-    const wagerList = this.props.listOfWagers
+    const wagerList = this.props.listOfWagers;
 
     const managedWagers = wagerList.filter(wager => {
-      return (wager.manager === this.state.metamask && !wager.complete)
-    })
+      return wager.manager === this.state.metamask && !wager.complete;
+    });
 
     const inTheseWagers = wagerList.filter(wager => {
-      return ((wager.side1).concat(wager.side2)).indexOf(this.state.metamask) > -1
-    })
+      return wager.side1.concat(wager.side2).indexOf(this.state.metamask) > -1;
+    });
 
-    console.log("managed: ", managedWagers)
+    console.log("managed: ", managedWagers);
     return (
       <div>
         <Segment inverted>
           <Header as="h2" icon textAlign="center">
-          <i className="ethereum icon circular"></i>
+            <i className="ethereum icon circular" />
             <Header.Content>
               <h2 className="ui blue header">
                 Hello {auth.currentUser.displayName}
@@ -68,7 +75,11 @@ class ProfilePage extends Component {
             <Header.Content>
               <Grid columns={4}>
                 <Grid.Column>
-                  <Button className="ui left floated primary button" circular onClick={this.goHome}>
+                  <Button
+                    className="ui left floated primary button"
+                    circular
+                    onClick={this.goHome}
+                  >
                     <Icon name="home" circular />
                   </Button>
                 </Grid.Column>
@@ -76,64 +87,112 @@ class ProfilePage extends Component {
             </Header.Content>
           </Header>
         </Segment>
-        <h2>
-          Wagers you are currently managing:
-        </h2>
-        <Grid columns={5}>
-          {managedWagers.map(wager =>
-            wager.complete ? null :
-              (
+
+
+        <Header as="h1" icon textAlign="center">
+          <Icon name="hourglass half" circular />
+          <Header.Content>Wager Status</Header.Content>
+        </Header>
+
+
+        <Grid columns={2}>
+          <Grid.Column>
+          <Header as='h2' textAlign='centered'>Wagers you are currently managing:</Header>
+          <Grid columns={3}>
+              {managedWagers.length ? (
+                managedWagers.map(
+                  wager =>
+                    wager.complete ? null : (
+                      <Grid.Column>
+                        <Card
+                          key={wager.address}
+                          className="ui segment centered"
+                        >
+                          <Image src={basketball} />
+                          <Card.Header />
+                          <Link
+                            to={`/wagers/${wager.address}`}
+                            key={wager.address}
+                            value={wager.address}
+                          >
+                            Click here to bet on
+                            <br />
+                            {wager.title}
+                          </Link>
+                          Ante: {wager.ante} Ether
+                          <br />
+                          Current Pot Size:{" "}
+                          {web3.utils.fromWei(wager.pot, "ether")} Ether
+                        </Card>
+                      </Grid.Column>
+                    )
+                )
+              ) : (
+                <div>
+                <Grid.Column />
                 <Grid.Column>
-                  <Card key={wager.address} className="ui segment centered">
-                    <Image src={basketball} />
-                    <Card.Header />
-                    <Link to={`/wagers/${wager.address}`} key={wager.address} value={wager.address}>
-                      Click here to bet on
-                    <br></br>
-                      {wager.title}
-                    </Link>
-                    Ante: {wager.ante} Ether
                   <br />
-                    Current Pot Size: {web3.utils.fromWei(wager.pot, 'ether')} Ether
-                </Card>
-                </Grid.Column>
-              ))}
-        </Grid>
-        <h2>
-          Wagers you are currently participating in:
-        </h2>
-        <Grid columns={5}>
-          {inTheseWagers.map(wager =>
-            wager.complete ? null :
-              (
+                  <br />
+                  <Header as='h3' textAlign='centered'>You are not currently managing any wagers.</Header>
+                  </Grid.Column>
+                  </div>
+              )}
+            </Grid>
+          </Grid.Column>
+          <Grid.Column>
+            <Header as='h2' textAlign='centered'>Wagers you are currently participating in:</Header>
+            <Grid columns={3}>
+              {inTheseWagers.length ? (
+                inTheseWagers.map(
+                  wager =>
+                    wager.complete ? null : (
+                      <Grid.Column>
+                        <Card
+                          key={wager.address}
+                          className="ui segment centered"
+                        >
+                          <Image src={basketball} />
+                          <Card.Header />
+                          <Link
+                            to={`/wagers/${wager.address}`}
+                            key={wager.address}
+                            value={wager.address}
+                          >
+                            Click here to bet on
+                            <br />
+                            {wager.title}
+                          </Link>
+                          Ante: {wager.ante} Ether
+                          <br />
+                          Current Pot Size:{" "}
+                          {web3.utils.fromWei(wager.pot, "ether")} Ether
+                        </Card>
+                      </Grid.Column>
+                    )
+                )
+              ) : (
+                <div>
+                <Grid.Column />
                 <Grid.Column>
-                  <Card key={wager.address} className="ui segment centered">
-                    <Image src={basketball} />
-                    <Card.Header />
-                    <Link to={`/wagers/${wager.address}`} key={wager.address} value={wager.address}>
-                      Click here to bet on
-                    <br></br>
-                      {wager.title}
-                    </Link>
-                    Ante: {wager.ante} Ether
-                  <br />
-                    Current Pot Size: {web3.utils.fromWei(wager.pot, 'ether')} Ether
-                </Card>
-                </Grid.Column>
-              ))}
+                  <br/>
+                  <br/>
+                  <Header as='h3' textAlign='centered'>You are not current participating in any wagers.</Header>
+                  </Grid.Column>
+                  </div>
+              )}
+            </Grid>
+          </Grid.Column>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = function (state, ownProps) {
+const mapStateToProps = function(state, ownProps) {
   return {
     currentUser: state.user,
     listOfWagers: state.wagers
   };
 };
 
-export default withRouter(connect(mapStateToProps, null)(ProfilePage))
-
-
+export default withRouter(connect(mapStateToProps, null)(ProfilePage));
